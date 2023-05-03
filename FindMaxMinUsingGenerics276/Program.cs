9d25353b3e95add9cd3817f79a4538bb1cb113e2
Sachinmahajana <sachinmahajan782@gmail.com>
2023-05-03 13:40:27 +0530

    [Sachin] Adding UC1-FindMaxint

--- FindMaxMinUsingGenerics276/Program.cs
+++ FindMaxMinUsingGenerics276/Program.cs
@@ -5,6 +5,16 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to The Findmaxmin Generics program");
+            Console.WriteLine("Select the program given below");
+            Console.WriteLine("\n1:Findmaxint");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Findmaxint.FindMaxintFunction(50, 20, 30);
+                    break;
+            }
+           
         }
     }
 }
