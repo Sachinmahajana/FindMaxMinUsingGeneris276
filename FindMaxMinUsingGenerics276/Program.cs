e27a2f08d11630ca384ad63049938fb019f12696
Sachinmahajana <sachinmahajan782@gmail.com>
2023-05-03 15:39:21 +0530

    [Sachin] Adding UC2-FindMaxMinFloat

--- FindMaxMinUsingGenerics276/Program.cs
+++ FindMaxMinUsingGenerics276/Program.cs
@@ -6,15 +6,17 @@
         {
             Console.WriteLine("Welcome to The Findmaxmin Generics program");
             Console.WriteLine("Select the program given below");
-            Console.WriteLine("\n1:Findmaxint");
+            Console.WriteLine("\n1:Findmaxint\n2:FindFloatmaxmin");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
                     Findmaxint.FindMaxintFunction(50, 20, 30);
                     break;
-            }
-           
+                case 2:
+                    FindmaxminFloat.FindMaxMinFloatFunction(50.3f,40.2f,30.4f);
+                    break;
+            }  
         }
     }
 }
