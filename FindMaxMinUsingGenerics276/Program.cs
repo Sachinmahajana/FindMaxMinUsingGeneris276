39c70e677143a25b431c65d847508bf7a33db99d
Sachinmahajana <sachinmahajan782@gmail.com>
2023-05-03 11:11:13 +0530

    [Sachin] Adding Welcome message

---
+++ FindMaxMinUsingGenerics276/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FindMaxMinUsingGenerics276
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to The Findmaxmin Generics program");
+        }
+    }
+}
