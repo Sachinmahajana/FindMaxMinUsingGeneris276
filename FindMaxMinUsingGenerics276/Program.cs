9af6abb93d8db77729b91bb40273b8c0ca3bd16f
Sachinmahajana <sachinmahajan782@gmail.com>
2023-05-03 16:25:33 +0530

    [Sachin] Adding UC3-Findmaxminstring

--- FindMaxMinUsingGenerics276/Program.cs
+++ FindMaxMinUsingGenerics276/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Welcome to The Findmaxmin Generics program");
             Console.WriteLine("Select the program given below");
-            Console.WriteLine("\n1:Findmaxint\n2:FindFloatmaxmin");
+            Console.WriteLine("\n1:Findmaxint\n2:FindFloatmaxmin\n3:FindMaxminstring");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -15,6 +15,12 @@
                     break;
                 case 2:
                     FindmaxminFloat.FindMaxMinFloatFunction(50.3f,40.2f,30.4f);
+                    break;
+                case 3:
+                    FindMaxminstring.FindMaxstringFunction("Apple","Peach","banana");
+                    break;
+                default:
+                    Console.WriteLine("Please select the program given below");
                     break;
             }  
         }
