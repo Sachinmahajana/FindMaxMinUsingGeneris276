d03dc746383810252e8e78ddcc7636710c254e8d
Sachinmahajana <sachinmahajan782@gmail.com>
2023-05-03 19:48:29 +0530

    [Sachin] Adding program

---
+++ FindMaxMinUsingGenerics276/FindMaximum.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumUsingGenerics
+{
+    internal class FindMaximum<T> where T : IComparable
+    {
+        public static void FindingMaximum(T input1, T input2, T input3) 
+        {
+            if (input1.CompareTo(input2) >= 0 && input1.CompareTo(input3) >= 0)
+            {
+                Console.WriteLine("Maximum is:" + input1);
+            }
+            else if (input2.CompareTo(input2) >= 0 && input2.CompareTo(input3) >= 0)
+            {
+                Console.WriteLine("Maximum is:" + input2);
+            }
+            else
+            {
+                Console.WriteLine("Maximum is:" + input3);
+            }
+        }
+
+    }
+}
--- FindMaxMinUsingGenerics276/Program.cs
+++ FindMaxMinUsingGenerics276/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace FindMaxMinUsingGenerics276
+﻿using FindMaximumUsingGenerics;
+
+namespace FindMaxMinUsingGenerics276
 {
     internal class Program
     {
@@ -6,7 +8,7 @@
         {
             Console.WriteLine("Welcome to The Findmaxmin Generics program");
             Console.WriteLine("Select the program given below");
-            Console.WriteLine("\n1:Findmaxint\n2:FindFloatmaxmin\n3:FindMaxminstring");
+            Console.WriteLine("\n1:Findmaxint\n2:FindFloatmaxmin\n3:FindMaxminstring\n4:Refactor1findmax");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -18,6 +20,11 @@
                     break;
                 case 3:
                     FindMaxminstring.FindMaxstringFunction("Apple","Peach","banana");
+                    break;
+                case 4:
+                    FindMaximum<int>.FindingMaximum( 50, 20, 30);
+                    FindMaximum<float>.FindingMaximum(60.3f, 40.2f, 20.2f);
+                    FindMaximum<string>.FindingMaximum("Skoda", "Swift", "Alto");
                     break;
                 default:
                     Console.WriteLine("Please select the program given below");
